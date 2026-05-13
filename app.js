2b8e4692fd9f0ed82edb6a49e1fb09d6bafed889
Alphen Kriston <96868417+alpken@users.noreply.github.com>
2026-05-13 15:58:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+console.log('Is this working?');
+
+let viz;
+
+// Tableau Public Dashboard URL
+const url = "https://public.tableau.com/views/A-B-Testing/Dashboard1?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
+
+const vizContainer = document.getElementById('vizContainer');
+
+const options = {
+    hideTabs: true,
+    height: "1000px",
+    width: "1200px",
+
+    onFirstInteraction: function () {
+        workbook = viz.getWorkbook();
+        activeSheet = workbook.getActiveSheet();
+
+        console.log("My dashboard is interactive");
+    }
+};
+
+// Create Tableau Viz
+function initViz() {
+
+    console.log('Executing the initViz function!');
+
+    viz = new tableau.Viz(vizContainer, url, options);
+}
+
+// Run when page loads
+document.addEventListener("DOMContentLoaded", initViz);
+
+// Buttons
+const exportPDF = document.getElementById('exportPDF');
+const exportImage = document.getElementById('exportImage');
+
+// Generate PDF
+function generatePDF() {
+    viz.showExportPDFDialog();
+}
+
+exportPDF.addEventListener("click", function () {
+    generatePDF();
+});
+
+// Generate Image
+function generateImage() {
+    viz.showExportImageDialog();
+}
+
+exportImage.addEventListener("click", function () {
+    generateImage();
+});
